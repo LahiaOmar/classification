437194c1176d8878b7084e2f3485c48e0bb9fde6
LahiaOmar <omar.lahia@gmail.com>
2018-08-05 20:47:02 +0100

    Naiv Bayes classifier class Not -- complete yet --

---
+++ Naive_Bayes_classifier/NaiveBayesClassifier.py
@@ -0,0 +1,52 @@
+# let coding = begin
+
+class NaiveBayesClassifier:
+  def __init__(self, dataSet):
+    self.dataSet = dataSet
+    self.size = len(dataSet)
+    self.mapping = {}
+    self.createMatrix()
+
+  def createMatrix(self):
+    self.mappingClass()
+    self.EVofClasses()
+  
+  def EVofClasses(self):
+    for key, value in self.mapping.items():
+      esps, vars = self.calculeEV(value)
+      print("esps of {0} : {1} and vars of {2} : {3}".format(key,esps,key,vars))
+    
+  def calculeEV(self, value):
+    espOfParam = []
+    varOfParam = []
+    si = len(value)
+    for i in range( len( value[0] ) ):
+      sum = 0
+      for j in range( si ):
+         sum += value[j][i]
+      espOfParam.append(sum/si)
+      esp = sum/si
+      sum = 0
+      for j in range( si ):
+        sum +=( ( value[j][i] - esp ) * ( value[j][i] - esp ) )
+      if si != 1 : 
+        varOfParam.append(sum/(si-1))
+      else :
+        varOfParam.append(sum)
+    return espOfParam,varOfParam
+
+  def mappingClass(self):
+    indColumnClasses = len(self.dataSet[0]) -1
+    for i in range(self.size):
+      clsName = self.dataSet[i][indColumnClasses]
+      row = self.dataSet[i]
+      if clsName not in self.mapping:
+        self.mapping[ clsName ] = []
+        self.mapping[ clsName ].append(row[0: indColumnClasses])
+      else:
+        self.mapping[ clsName ].append(row[0: indColumnClasses]) 
+    
+    print("mapping obj : ", self.mapping)
+
+
+
---
+++ Naive_Bayes_classifier/main.py
@@ -0,0 +1,8 @@
+# let coding = begin
+
+from NaiveBayesClassifier import NaiveBayesClassifier
+
+dataSet = [[1,1,1,4],[1,1,1,3],[2,1,1,3]]
+test = []
+
+nbc = NaiveBayesClassifier(dataSet)
